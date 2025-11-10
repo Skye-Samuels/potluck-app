53dcb794013066711cf5bc7a636bd75bd1229415
Skye Samuels <129140642+Skye-Samuels@users.noreply.github.com>
2025-11-09 17:38:27 -0800

    Slight fix to countdown caused by last commit.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
 	import '../app.css';
-	import favicon from '/favicon.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" />
 </svelte:head>
 
 {@render children()}
