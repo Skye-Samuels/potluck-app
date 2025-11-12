393de4c44f2dcf5c2289469a49008b94a860f6e0
Skye Samuels <129140642+Skye-Samuels@users.noreply.github.com>
2025-11-12 09:22:25 -0800

    Fixed scoreboard page.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,15 +4,23 @@ import { TARGET_ISO } from '$lib/config.js';
 export function handle({ event, resolve }) {
 	const now = new Date();
 	const target = new Date(TARGET_ISO);
+	const path = event.url.pathname;
 
 	if (now < target) {
-		const path = event.url.pathname;
 		if (
 			!path.startsWith('/countdown') &&
 			!path.startsWith('/_app') &&
 			!path.startsWith('/favicon')
 		) {
 			throw redirect(302, '/countdown');
+		}
+	} else {
+		if (
+			!path.startsWith('/scoreboard') &&
+			!path.startsWith('/_app') &&
+			!path.startsWith('/favicon')
+		) {
+			throw redirect(302, '/scoreboard');
 		}
 	}
 
--- src/lib/config.js
+++ src/lib/config.js
@@ -2,4 +2,4 @@
 export const TARGET_ISO = '2025-12-05T15:00:00';
 
 // Uncomment and edit for testing dif
-// export const TARGET_ISO = "2025-11-09T15:18:00";
+// export const TARGET_ISO = "2025-10-09T15:18:00";
